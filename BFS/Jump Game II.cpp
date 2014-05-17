111fe06ae489a49a15434c68b9ea99650edca6eb
ching <chingc@outlook.com>
2014-05-17 12:50:13 +0800

    update

--- BFS/Jump Game II.cpp
+++ BFS/Jump Game II.cpp
@@ -13,28 +13,19 @@ The minimum number of jumps to reach the last index is 2. (Jump 1 step from inde
 
 class Solution {
 public:
-	int jump(int A[], int n) {
-		if (n == 0 || n == 1) return 0;
-		int end = 0;
-		int level = 0;
-		//表示这一层最大能达到的index
-		int last = 0;
-		for (int i = 0; i <= end; i++)
-		{
-			//如果超过这一次最大能达到的index，那么level就要增加1
-			if (i > last)
-			{
-				level++;
-				//更新level的时候要更新这一层能达到的最大index
-				last = end;
-			}
-			//每个元素都要去更新这一层能达到的最大的index
-			end = max(end, A[i] + i);
-			//如果当前end大于n-1,代表已经达到最后一个，返回当前level+1
-			if (end >= n - 1)
-			{
-				return level + 1;
-			}
-		}
-	}
+    int jump(int A[], int n) {
+        int nextLevelMax = 0;
+        int steps = 0;
+        int thisLevelMax = 0;
+        for(int i = 0 ;i < n;i++)
+        {
+            if(i > thisLevelMax)
+            {
+                steps++;
+                thisLevelMax = nextLevelMax;
+            }
+            nextLevelMax = max(nextLevelMax,i+A[i]);
+        }
+        return steps;
+    }
 };
--- Stack/Largest Rectangle in Histogram.cpp
+++ Stack/Largest Rectangle in Histogram.cpp
@@ -1,5 +1,6 @@
 /*
- Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, 
+find the area of largest rectangle in the histogram.
 
 http://www.leetcode.com/wp-content/uploads/2012/04/histogram.png
 Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
