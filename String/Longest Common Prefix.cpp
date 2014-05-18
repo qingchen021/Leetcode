a4af77d8a6ddea9624170fb0cba18478af17ddb2
qing <qingchen021@users.noreply.github.com>
2014-05-18 14:21:36 +0800

    Update Longest Common Prefix.cpp

--- String/Longest Common Prefix.cpp
+++ String/Longest Common Prefix.cpp
@@ -7,54 +7,17 @@ Write a function to find the longest common prefix string amongst an array of st
 
 class Solution {
 public:
-	string longestCommonPrefix(vector<string> &strs) 
-	{
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		if( strs.size() == 0)
-		{
-			return "";
-		}
-		char ret[1000]={0};
-		if(strs.size()==1)
-			return strs[0];
-		int p = 0;
-		bool flag = true;
-
-		while (flag)
-		{
-			char candidate =' ';
-			if(strs[0].size() > p)
-			{
-				candidate = strs[0][p];
-			}
-			else
-			{
-				break;
-			}
-			for(int i = 1; i < strs.size(); i++)
-			{
-				if( strs[i].size() > p)
-				{
-					if( strs[i][p]== candidate)
-					{
-					}
-					else
-					{
-						flag = false;
-						break;
-					}
-				}
-				else
-				{
-					flag = false;
-					break;
-				}
-			}
-			if(flag)
-				ret[p++]=candidate;
-		}
-		ret[p]='\0';
-		return ret;
-	}
+    string longestCommonPrefix(vector<string> &strs) {
+        if(strs.size() == 0)
+            return "";
+        for(int i = 0 ; i < strs[0].length();i++)
+            for(int j = 1 ; j < strs.size();j++)
+            {
+                if(i >= strs[j].length() || strs[0][i] != strs[j][i])
+                {
+                    return strs[0].substr(0,i);
+                }
+            }
+        return strs[0];
+    }
 };
