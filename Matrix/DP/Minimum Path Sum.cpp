241b45c08cbb7838412ba8cfaeb8fa9d80414884
qing <qingchen021@users.noreply.github.com>
2014-05-19 06:58:42 +0800

    Update Minimum Path Sum.cpp

--- Matrix/DP/Minimum Path Sum.cpp
+++ Matrix/DP/Minimum Path Sum.cpp
@@ -6,25 +6,19 @@ Note: You can only move either down or right at any point in time.
 
 class Solution {
 public:
-	int minPathSum(vector<vector<int> > &grid) {
-		int DP[1000][1000];
-		int rows = grid.size();
-		int cols = grid[0].size();
-		//自顶向下，每个数，要么从左边来，要么从上边来，一次遍历所有的矩阵，
-		//非常基础的DP的题目
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < cols; j++)
-			{
-				int left = INT_MAX, top = INT_MAX;
-				if (i > 0)
-					top = DP[i - 1][j];
-				if (j >0)
-					left = DP[i][j - 1];
-				if (i == 0 && j == 0)
-					DP[0][0] = grid[0][0];
-				else
-					DP[i][j] = min(left, top) + grid[i][j];
-			}
-		return DP[rows - 1][cols - 1];
-	}
+    int minPathSum(vector<vector<int> > &grid) {
+        vector<vector<int> > dp(grid.size(),vector<int>(grid[0].size(),0));
+        for(int i = 0 ;i < grid.size();i++)
+            for(int j = 0 ; j < grid[0].size();j++)
+            {
+                if(i>0 && j>0)
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1]);
+                else if(i>0)
+                    dp[i][j]= dp[i-1][j];
+                else if(j>0)
+                    dp[i][j]= dp[i][j-1];
+                dp[i][j] += grid[i][j];
+            }
+        return dp[grid.size()-1][grid[0].size()-1];
+    }
 };
