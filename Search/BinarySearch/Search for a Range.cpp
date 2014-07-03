2adce450c1d23ba83cc301f21304f9abd5ff8120
ching <chingc@outlook.com>
2014-07-04 07:59:09 +0800

    clear solution

--- Search/BinarySearch/Search for a Range.cpp
+++ Search/BinarySearch/Search for a Range.cpp
@@ -10,68 +10,54 @@ Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4]. 
 */
 
- class Solution
- {
-
- public:
-
-	 vector<int> searchRange(int A[], int n, int target)
-	 {
-
-
-		 vector<int> ret;
-		 int first = BinarySearch(A, 0, n - 1, target, -1);
-		 if (first == -1)
-		 {
-			 ret.push_back(-1);
-			 ret.push_back(-1);
-			 return ret;
-		 }
-		 else
-		 {
-			 ret.push_back(first);
-			 if (first == n - 1)
-				 ret.push_back(first);
-			 else{
-				 int second = BinarySearch(A, first + 1, n - 1, target, 1);
-				 if (second == -1)
-					 ret.push_back(first);
-				 else
-					 ret.push_back(second);
-			 }
-			 return ret;
-		 }
-
-	 }
-
-	 int BinarySearch(int A[], int first, int last, int target, int direction)
-	 {
-		 int begin = first, end = last;
-		 while (begin <= end)
-		 {
-			 int mid = (begin + end) / 2;
-			 if (A[mid] == target)
-			 {
-				 if ((direction > 0 && mid == last) || (direction < 0 && mid == first))
-					 return mid;
-				 else if (A[mid + direction] != target)
-					 return mid;
-				 else
-				 {
-					 if (direction > 0)
-						 begin = mid + 1;
-					 else
-						 end = mid - 1;
-				 }
-
-			 }
-			 else if (A[mid] > target)
-			 {
-				 end = mid - 1;
-			 }
-			 else
-				 begin = mid + 1;
-		 }
-		 return -1;
-	 }
- };
+class Solution {
+public:
+    vector<int> searchRange(int A[], int n, int target) {
+        int lowBound = -1, highBound = -1;
+        int low = 0 , high = n-1;
+        vector<int> ret ;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(A[mid]>=target)
+            {
+                lowBound = mid;
+                high = mid -1;
+            }
+            else
+            {
+                low = mid +1;
+            }
+        }
+        
+        if(lowBound!=-1)
+        {
+            if(A[lowBound]!=target)
+                lowBound = -1;
+        }
+        low = 0;
+        high = n-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(A[mid]<=target)
+            {
+                highBound = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        
+         if(highBound!=-1)
+        {
+            if(A[highBound]!=target)
+                highBound = -1;
+        }
+        ret.push_back(lowBound);
+        ret.push_back(highBound);
+        return ret;
+    }
+};
