55e8164af9d1020704eecaaecd3d6b7f43fb0e50
ching <chingc@outlook.com>
2014-07-05 11:30:15 +0800

    update

--- Sort/Sort List.cpp
+++ Sort/Sort List.cpp
@@ -1,45 +1,50 @@
 /*
 Sort a linked list in O(n log n) time using constant space complexity.
 */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode *sortList(ListNode *head) {
-		if (head == NULL || head->next == NULL)
-			return head;
-		ListNode  dummy(0);
-		dummy.next = head;
-		ListNode * slow = &dummy, *fast = &dummy;
-		//here need to check fast->next != NULL
-		while (slow && fast && fast->next)
-		{
-			slow = slow->next;
-			fast = fast->next;
-			fast = fast->next;
-		}
-		ListNode * another = slow->next;
-		slow->next = NULL;
-		ListNode * first = sortList(head);
-		ListNode * second = sortList(another);
-		ListNode * pre = &dummy;
-		while (first && second)
-		{
-			if (first->val < second->val)
-			{
-				pre->next = first;
-				first = first->next;
-			}
-			else
-			{
-				pre->next = second;
-				second = second->next;
-			}
-			pre = pre->next;
-		}
-		if (first)
-			pre->next = first;
-		if (second)
-			pre->next = second;
-		return dummy.next;
-	}
-
+    ListNode *sortList(ListNode *head) {
+        if(head==NULL||head->next==NULL)
+            return head;
+        ListNode * slow = head, * fast = head;
+        while(fast->next&&fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;;
+        }
+        
+        ListNode * tmp = slow->next;
+        slow->next = NULL;
+        ListNode * first = sortList(head);
+        ListNode * second = sortList(tmp);
+        ListNode dummyNode(INT_MIN);
+        ListNode * pre = &dummyNode;
+        while(first&&second)
+        {
+            if(first->val < second->val)
+            {
+                pre->next =first;
+                first=first->next;
+            }
+            else
+            {
+                pre->next = second;
+                second = second->next;
+            }
+            pre = pre->next;
+        }
+        if(first)
+            pre->next = first;
+        if(second)
+            pre->next = second;
+        return dummyNode.next;
+    }
 };
