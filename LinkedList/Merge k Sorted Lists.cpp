93c3c6f03deb7b6f1deadc7ccba0fb4f0f01781a
ching <chingc@outlook.com>
2014-07-16 08:08:54 +0800

    correct solution
    correct solution

--- LinkedList/Merge k Sorted Lists.cpp
+++ LinkedList/Merge k Sorted Lists.cpp
@@ -2,39 +2,59 @@
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity. 
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
-#define MAX  2147483647
 public:
-	ListNode *mergeKLists(vector<ListNode *> &lists) {
-		ListNode  dummyNode = ListNode(MAX);
-		ListNode * last = & dummyNode;
-		int size = lists.size();
-		if(size == 0)
-			return NULL;
-		int curIndex = -1;
-		do{
-			curIndex = -1;
-			int curMin = MAX;
-			for(int i = 0 ;i < lists.size(); i++)
-			{	
-				if(lists[i]!=NULL)
-				{
-					int tmp = lists[i] ->val;
-					if( tmp <= curMin)
-					{
-						curMin = tmp;
-						curIndex = i;
-					}
-				}
-			}
-			if( curIndex != -1)
-			{	
-				last->next = lists[curIndex];
-				last = last->next;
-				lists[curIndex]= lists[curIndex]->next;
-			}
-		}while(curIndex != -1);
-		last->next = NULL;
-		return dummyNode.next;
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        int size = lists.size();
+	if(size == 0)
+		return NULL;
+	while(size>1)
+	{
+		int n = (size+1)/2;
+		for(int i = 0;i < n;i++)
+		{
+			lists[i] = merge(lists[i],lists[size-i-1]);
+		}
+		size = n;
+	};
+	return lists[0];
+    }
+    ListNode * merge(ListNode * first, ListNode *second)
+   {
+	if(first == second)
+		return first;
+	ListNode dummyNode (INT_MIN), * pre;
+	pre = &dummyNode;
+	while(first && second)
+	{
+		if(first->val > second->val)
+		{
+			pre->next = second;
+			second= second->next;
+		}
+		else 
+		{
+			pre->next = first;
+			first = first->next;	
+		}
+		pre = pre->next;
 	}
+	if(first)
+	{
+		pre->next = first;
+	}
+	if(second)
+	{
+		pre->next = second;
+	}
+	return dummyNode.next;
+   }
 };
