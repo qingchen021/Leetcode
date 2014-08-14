d56498b994f46e4ff4366e0bdc8a304ee54d5608
ching <chingc@outlook.com>
2014-08-14 21:47:03 +0800

    Largest Rectangle in Histogram
    Largest Rectangle in Histogram

--- Stack/Largest Rectangle in Histogram.cpp
+++ Stack/Largest Rectangle in Histogram.cpp
@@ -14,56 +14,52 @@ return 10.
 */
 
 class Solution {
-	struct Node
-	{
-		int h, p;
-		Node(int height, int pos):h(height),p(pos){}
-	};
+    struct Point
+    {
+        int x ,y,p;
+        Point(int xx,int yy):x(xx),y(yy)
+        {
+            
+        }
+    };
 public:
-    //主要考察的是当前的高度左右延伸的距离，这可以用栈来完成
-	int largestRectangleArea(vector<int> &height) {
-		int ret = 0;
-		stack<Node> hStack;
-		for(int i = 0; i < height.size() ;i++)
-		{
-			if(! hStack.empty())
-			{
-				Node top = hStack.top();
-				Node n(height[i],0);
-				//如果当前元素比栈顶元素小，那么栈顶元素最右边就是当前index的前一个了
-				if(top.h > height[i])
-				{
-				    //这里要确定所有栈内的元素都比当前push进去的元素要小
-					while (top.h > height[i])
-					{
-					    //每次pop之前更新当前元素最左端位置
-						n.p = top.p;
-						ret = max(ret , top.h *(i - top.p));
-						hStack.pop();
-						if(!hStack.empty())
-							top = hStack.top();
-						else
-						{
-							break;
-						}
-					}		
-				}
-				else
-				//如果当前原始比栈顶元素大，那么当前元素最长左边就是自己index了，
-					n.p = i;
-				hStack.push(n);				
-			}
-			else
-				hStack.push(Node(height[i],i));
-		}
-		int last = height.size() -1;
-		//如果遍历完，栈里面还有元素，代表，它们最右边的边界就是len－1
-		while(!hStack.empty())
-		{
-			Node top = hStack.top();
-			ret = max(ret , top.h *(last - top.p + 1));
-			hStack.pop();
-		}
-		return ret;
-	}
+    int largestRectangleArea(vector<int> &height) {
+        stack<Point> stackS;
+        int largest = 0;
+        for(int i = 0;i< height.size();i++)
+        {
+            int mostLeft = i;
+            while(true)
+            {
+                if(!stackS.empty())
+                {
+                    Point t = stackS.top();
+                    if(t.y > height[i])
+                    {
+                        mostLeft = t.x;
+                        largest = max(largest,t.y *(i-t.x));
+                        stackS.pop();
+                    }
+                    else
+                    {
+                        stackS.push(Point(mostLeft,height[i]));
+                        break;
+                    }
+                }
+                else
+                {
+                    stackS.push(Point(mostLeft,height[i]));
+                    break;
+                }
+            }
+        }
+        int mostRight = height.size()-1;
+        while(!stackS.empty())
+        {
+             Point top = stackS.top();
+             stackS.pop();
+             largest = max(largest,top.y *(mostRight - top.x +1));
+        }
+        return largest;
+    }
 };
