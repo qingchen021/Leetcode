79182d1fbe233d4e4f45685296b1ffb1dddbc1e3
qing <qingchen021@gmail.com>
2014-09-02 21:18:35 +0800

    Multiply Strings
    Multiply Strings

--- Mathematics/Multiply Strings.cpp
+++ Mathematics/Multiply Strings.cpp
@@ -5,37 +5,31 @@ Note: The numbers can be arbitrarily large and are non-negative.
 */
 
 class Solution
- {
-
- public:
-
-	 string multiply(string num1, string num2)
-	 {
-		 if (num1 == "0" || num2 == "0")
-			 return "0";
-		 vector<int> result(num1.size() + num2.size(), 0);
-		 for (int i = 0; i < num1.size(); i++)
-		 for (int j = 0; j<num2.size(); j++)
-		 {
-			 result[i + j + 1] += (num1[i] - '0')*(num2[j] - '0');
-		 }
-		 for (int i = num1.size() + num2.size() - 1; i>0; i--)
-		 {
-			 if (result[i]>9)
-			 {
-				 result[i - 1] += result[i] / 10;
-				 result[i] %= 10;
-			 }
-		 }
-		 string ret = "";
-		 int i = 0;
-		 while (result[i] == 0)
-			 i++;
-		 for (; i<num1.size() + num2.size(); i++)
-		 {
-			 ret.append(1, result[i] + '0');
-		 }
-		 return ret;
-	 }
-
- };
+{
+public:
+	string multiply(string num1, string num2)
+	{
+		if (num1 == "0" || num2 == "0")
+			return "0";
+		vector<int> res(num1.length() + num2.length(), 0);
+		for (int i = num1.length(); i > 0; i--)
+			for (int j = num2.length(); j > 0; j--)
+			{
+				int c = (num1[i - 1] - '0')*(num2[j - 1] - '0');
+				res[i + j - 1] += c;
+			}
+		for (int i = num1.length() + num2.length() - 1; i > 0; i--)
+		{
+			int c = res[i] / 10;
+			res[i - 1] += c;
+			res[i] %= 10;
+		}
+		int i = 0;
+		while (i < num1.length() + num2.length() && res[i] == 0)
+			i++;
+		string ret = "";
+		while (i<num1.length() + num2.length())
+			ret.append(1, res[i++] + '0');
+		return ret;
+	}
+};
