7c3b273e1d5927377a7fb3ec032603ce5dc2670b
qing <qingchen021@gmail.com>
2014-09-25 22:15:02 +0800

    update

--- Search/BinarySearch/Search in Rotated Sorted Array.cpp
+++ Search/BinarySearch/Search in Rotated Sorted Array.cpp
@@ -9,56 +9,31 @@ You may assume no duplicate exists in the array.
 */
 
  class Solution {
- public:
-	 int search(int A[], int n, int target) {
-		 int begin = 0, end = n - 1;
-		 while (begin <= end)
-		 {
-			 int mid = (begin + end) / 2;
-			 if (target == A[mid])
-				 return mid;
-			 else if (target > A[mid])
-			 {
-				 if( A[end] >= A[begin])
-				{
-					begin = mid+1;
-				}
-				else
-				{
-					if( A[mid] < A[begin])
-					{
-						if( target > A[end])
-							end = mid -1;
-						else
-							begin = mid+1;
-					}
-					else
-						begin = mid+1;
-				}
-			 }
-			 else
-			 {
-				 if( A[end] >= A[begin])
-				{
-					end = mid-1;
-				}
-				else
-				{
-					if(A[mid] < A[begin])
-					{
-						end = mid-1;							
-					}
-					else
-					{
-						if( target < A[begin])
-							begin = mid+1;
-						else
-							end = mid -1;
-					}
-				}
-			 }
-
-		 }
-		 return -1;
-	 }
- };
+public:
+    int search(int A[], int n, int target) {
+        int begin = 0 , end = n -1 ;
+        while(begin <= end)
+        {
+        	int mid = end - (end - begin)/2;
+        	if(A[mid] == target)
+        		return mid;
+        	if(A[mid] > A[begin])
+        	{
+        		if(target < A[mid] && target >= A[begin])
+        			end = mid -1;
+        		else
+        			begin = mid + 1;
+        	}
+        	else if(A[mid] < A[begin])
+        	{
+        		if(target > A[mid] && target <= A[end])
+        			begin = mid + 1;
+        		else
+        			end = mid -1;
+        	}
+        	else
+        		begin++;
+        }
+        return -1;
+    }
+};
