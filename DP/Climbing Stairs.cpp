f6e9a8ba1e8625c8bf8b5f63efe1b0ab413142ac
qing <qingchen021@gmail.com>
2014-09-29 07:44:22 +0800

    update

--- DP/Climbing Stairs.cpp
+++ DP/Climbing Stairs.cpp
@@ -4,16 +4,19 @@ You are climbing a stair case. It takes n steps to reach to the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
 */
 
-class Solution {
+class Solution 
+{
 public:
-    int climbStairs(int n) {
-        vector<int> stairs (n+1,0);
-        stairs[1] = 1;
-        stairs[2] = 2;
-        for(int i = 3 ;i <= n ;i++)
+    int climbStairs(int n) 
+    {
+        if(n == 1)
+            return 1;
+        int pre = 1, prePre = 2;
+        for(int i = 3;i<=n;i++)
         {
-        	stairs[i] = stairs[i-1] + stairs[i-2];
+            pre = prePre + pre;
+            swap(pre,prePre);
         }
-        return stairs[n];
+        return max(prePre,pre);
     }
 };
