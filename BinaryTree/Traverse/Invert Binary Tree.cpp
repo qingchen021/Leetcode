63085e8f90746f8187c71eb4b8ee08d038e006fe
qing <qingchen021@gmail.com>
2015-06-15 19:52:49 +0800

    update

---
+++ BinaryTree/Traverse/Invert Binary Tree.cpp
@@ -0,0 +1,45 @@
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+    Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+            return NULL;
+        TreeNode * l = root->left;
+        root->left = root->right;
+        root->right = l;
+        invertTree(root->right);
+        invertTree(root->left);
+        return root;
+    }
+};
